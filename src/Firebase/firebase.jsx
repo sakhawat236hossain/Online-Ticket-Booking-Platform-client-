e57039bbd12db8f0e339dfa0bc2d6d636228c0f2
sakhawat236hossain <hmdsakhawat236@gmail.com>
2025-12-07 02:04:12 +0600

    firebase setupe

--- src/Firebase/firebase.jsx
+++ src/Firebase/firebase.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
 
-const firebase = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+import { initializeApp } from "firebase/app";
+
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-export default firebase;
+export default firebaseConfig;
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
